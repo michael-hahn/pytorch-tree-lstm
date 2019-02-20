58c033414ac213528126d29ca52536e66b73d49a
Jordan Dawe <jordan.dawe@unbounce.com>
2019-02-19 16:19:19 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorch-tree-lstm",
+    version="0.1.0",
+    author="Jordan Dawe",
+    author_email="jordan.dawe@unbounce.com",
+    description="A Tree-LSTM model package for PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unbounce/pytorch-tree-lstm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
